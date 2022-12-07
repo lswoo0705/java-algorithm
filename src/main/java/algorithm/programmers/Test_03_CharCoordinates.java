ef33ed943c30b649fb3c0dca6a2a63892fdbb643
sangwoo lee <lswoo0705@gmail.com>
2022-12-07 14:41:48 +0900

    알고리즘 문제 풀이

---
+++ src/main/java/algorithm/programmers/Test_03_CharCoordinates.java
@@ -0,0 +1,36 @@
+package algorithm.programmers;
+
+public class Test_03_CharCoordinates {
+    public int[] solution(String[] keyinput, int[] board) {
+        int x = 0;
+        int y = 0;
+        int MaxX = (board[0] - 1) / 2;
+        int MaxY = (board[1] - 1) / 2;
+        for(String input : keyinput){
+            switch (input) {
+                case "up":
+                    if (y < MaxY) {
+                        y++;
+                    }
+                    break;
+                case "down":
+                    if (y > -MaxY) {
+                        y--;
+                    }
+                    break;
+                case "left":
+                    if (x > -MaxX) {
+                        x--;
+                    }
+                    break;
+                case "right":
+                    if (x < MaxX) {
+                        x++;
+                    }
+                    break;
+            }
+        }
+        int[] coordinate = new int[]{x, y};
+        return coordinate;
+    }
+}
