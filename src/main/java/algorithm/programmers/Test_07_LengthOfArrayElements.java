5f42a97db176107f11b9608dab01d419e3103858
sangwoo lee <lswoo0705@gmail.com>
2022-12-12 09:36:24 +0900

    알고리즘 문제 풀이 07

---
+++ src/main/java/algorithm/programmers/Test_07_LengthOfArrayElements.java
@@ -0,0 +1,19 @@
+package algorithm.programmers;
+
+public class Test_07_LengthOfArrayElements {
+    public int[] solution(String[] strlist) {
+        int[] wordLength= new int[strlist.length];
+        int word = strlist.length;
+        for (int i = 0; i < word; i++) {
+            wordLength[i] = strlist[i].length();
+        }
+        return wordLength;
+    }
+
+    public static void main(String[] args) {
+        Test_07_LengthOfArrayElements sol = new Test_07_LengthOfArrayElements();
+        String[] input = {"We", "are", "the", "world!"};
+
+        System.out.println(sol.solution(input));
+    }
+}
