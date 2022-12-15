111f3e90d37ec917731bf4b93efabace7258fa35
sangwoo lee <lswoo0705@gmail.com>
2022-12-15 09:05:18 +0900

    알고리즘 문제 풀이 10

---
+++ src/main/java/algorithm/programmers/Test_10_SplitPizza.java
@@ -0,0 +1,13 @@
+package algorithm.programmers;
+
+public class Test_10_SplitPizza {
+    public int solution(int slice, int n) {
+        int answer = 0;
+        if (n % slice != 0) {
+            answer += n / slice + 1;
+        } else {
+            answer += n / slice;
+        }
+        return answer;
+    }
+}
