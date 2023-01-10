75c54773a0c9710af36f86fced5b4d31f3ae3a5a
sangwoo lee <lswoo0705@gmail.com>
2023-01-10 20:50:30 +0900

    알고리즘 문제 풀이 34

---
+++ src/main/java/algorithm/programmers/Test_31to40/Test_34_SortString.java
@@ -0,0 +1,17 @@
+package algorithm.programmers.Test_31to40;
+
+import java.util.Arrays;
+
+public class Test_34_SortString {
+    public int[] solution(String my_string) {
+        String intString = my_string.replaceAll("[^0-9]", "");
+
+        int[] answer = new int[intString.length()];
+
+        for (int i = 0; i < intString.length(); i++) {
+            answer[i] = Integer.parseInt(intString.substring(i, 1 + i));
+        }
+        Arrays.sort(answer);
+        return answer;
+    }
+}
