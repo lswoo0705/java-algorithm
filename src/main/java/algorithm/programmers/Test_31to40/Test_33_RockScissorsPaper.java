b6d86411ebd1eef689e9f184711d4d363b04b004
sangwoo lee <lswoo0705@gmail.com>
2023-01-10 10:40:24 +0900

    알고리즘 문제 풀이 33

--- src/main/java/algorithm/programmers/Test_31to40/Test_33_RockScissorsPaper.java
+++ src/main/java/algorithm/programmers/Test_31to40/Test_33_RockScissorsPaper.java
@@ -1,6 +1,25 @@
 package algorithm.programmers.Test_31to40;
 
 public class Test_33_RockScissorsPaper {
+
+//    char[] cRsp = rsp.toCharArray();
+//    StringBuilder answer = new StringBuilder();
+//
+//        for(int i=0; i < cRsp.length; i++) {
+//        switch(cRsp[i]) {
+//            case '0' :
+//                answer.append("5");
+//                break;
+//            case '2' :
+//                answer.append("0");
+//                break;
+//            case '5' :
+//                answer.append("2");
+//                break;
+//        }
+//    }
+//        return answer.toString();
+
     public String solution(String rsp) {
         String answer = "";
         for (int i = 0; i < rsp.length(); i++) {
