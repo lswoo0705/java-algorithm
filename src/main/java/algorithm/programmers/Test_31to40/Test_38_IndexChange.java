4fccbf2e644ae03b331a5646544fe50105115c76
sangwoo lee <lswoo0705@gmail.com>
2023-04-17 20:21:47 +0900

    알고리즘 문제 풀이 38

--- src/main/java/algorithm/programmers/Test_31to40/Test_38_IndexChange.java
+++ src/main/java/algorithm/programmers/Test_31to40/Test_38_IndexChange.java
@@ -1,9 +1,22 @@
 package algorithm.programmers.Test_31to40;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Test_38_IndexChange {
     public String solution(String my_string, int num1, int num2) {
+        // 각 인덱스에 해당하는 문자를 저장
+        char str1 = my_string.charAt(num1);
+        char str2 = my_string.charAt(num2);
 
-        String answer = "";
+        // 문자열을 문자 배열로 바꾼 뒤 값 입력
+        char[] myChar = my_string.toCharArray();
+        myChar[num1] = str2;
+        myChar[num2] = str1;
+
+        // 문자 배열을 문자열로 변환
+        String answer = String.valueOf(myChar);
+
         return answer;
     }
 
