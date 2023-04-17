7d988277e7f40a1fcc15353fb57b5fedaecadfd0
sangwoo lee <lswoo0705@gmail.com>
2023-04-17 19:25:17 +0900

    merge

---
+++ src/main/java/algorithm/programmers/Test_31to40/Test_38_IndexChange.java
@@ -0,0 +1,17 @@
+package algorithm.programmers.Test_31to40;
+
+public class Test_38_IndexChange {
+    public String solution(String my_string, int num1, int num2) {
+
+        String answer = "";
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Test_38_IndexChange sol = new Test_38_IndexChange();
+        String my_string = "hello";
+        int num1 = 1;
+        int num2 = 2;
+        System.out.println(sol.solution(my_string, num1, num2));
+    }
+}
