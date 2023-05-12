cab79ddb6dd5fc2124cbc524431806045c60b84e
sangwoo lee <lswoo0705@gmail.com>
2023-05-12 13:22:26 +0900

    알고리즘 문제 풀이 40

---
+++ src/main/java/algorithm/programmers/Test_31to40/Test_40_Pizza.java
@@ -0,0 +1,20 @@
+package algorithm.programmers.Test_31to40;
+
+public class Test_40_Pizza {
+    public int solution(int n) {
+        int answer = 0;
+        for (int i = 1; i <= 6 * n; i++) {
+            if (6 * i % n == 0) {
+                answer = i;
+                break;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Test_40_Pizza sol = new Test_40_Pizza();
+        int n = 10;
+        System.out.println(sol.solution(n));
+    }
+}
