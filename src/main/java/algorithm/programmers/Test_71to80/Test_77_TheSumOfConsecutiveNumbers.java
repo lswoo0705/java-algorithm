a0bec764ada6ffcd73b0e99e8e9c3272a6ea7092
sangwoo lee <lswoo0705@gmail.com>
2023-09-06 13:50:21 +0900

    알고리즘 문제 풀이 77

---
+++ src/main/java/algorithm/programmers/Test_71to80/Test_77_TheSumOfConsecutiveNumbers.java
@@ -0,0 +1,23 @@
+package algorithm.programmers.Test_71to80;
+
+public class Test_77_TheSumOfConsecutiveNumbers {
+    public int[] solution(int num, int total) {
+        int[] answer = new int[num];
+        int centerNum; // 중앙값
+        int downNum; // 내려가야 하는 수
+
+        if (total % num == 0) {
+            centerNum = total / num;
+            downNum = num / 2;
+        } else {
+            centerNum = total / num;
+            downNum = num / 2 - 1;
+        }
+
+        for (int i = 0; i < num; i++) {
+            answer[i] = centerNum - downNum + i;
+        }
+
+        return answer;
+    }
+}
