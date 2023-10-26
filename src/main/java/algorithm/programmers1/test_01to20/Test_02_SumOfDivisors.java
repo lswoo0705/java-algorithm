7705a7df82241ba8c17d60e9e2d053c38ec2bb1b
sangwoo lee <lswoo0705@gmail.com>
2023-10-26 13:36:00 +0900

    프로그래머스 1-2

---
+++ src/main/java/algorithm/programmers1/test_01to20/Test_02_SumOfDivisors.java
@@ -0,0 +1,13 @@
+package algorithm.programmers1.test_01to20;
+
+public class Test_02_SumOfDivisors {
+    public int solution(int n) {
+        int answer = 0;
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                answer += i;
+            }
+        }
+        return answer;
+    }
+}
