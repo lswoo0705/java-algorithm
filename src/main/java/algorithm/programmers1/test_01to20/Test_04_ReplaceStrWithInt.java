39ec19f2cf615bff3fa5e0f3d676214515454790
sangwoo lee <lswoo0705@gmail.com>
2023-10-30 19:27:45 +0900

    프로그래머스 1-4

---
+++ src/main/java/algorithm/programmers1/test_01to20/Test_04_ReplaceStrWithInt.java
@@ -0,0 +1,8 @@
+package algorithm.programmers1.test_01to20;
+
+public class Test_04_ReplaceStrWithInt {
+    public int solution(String s) {
+        int answer = Integer.parseInt(s);
+        return answer;
+    }
+}
