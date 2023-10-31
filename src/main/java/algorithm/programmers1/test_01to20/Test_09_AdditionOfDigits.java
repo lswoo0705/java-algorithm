659e8e3eb465c59308cc61328482c476070248f6
sangwoo lee <lswoo0705@gmail.com>
2023-10-31 10:41:34 +0900

    프로그래머스 1-9

---
+++ src/main/java/algorithm/programmers1/test_01to20/Test_09_AdditionOfDigits.java
@@ -0,0 +1,13 @@
+package algorithm.programmers1.test_01to20;
+
+public class Test_09_AdditionOfDigits {
+    public int solution(int n) {
+        int answer = 0;
+        String[] strArr = String.valueOf(n).split("");
+        for (int i = 0; i < strArr.length; i++) {
+            answer += Integer.parseInt(strArr[i]);
+        }
+
+        return answer;
+    }
+}
