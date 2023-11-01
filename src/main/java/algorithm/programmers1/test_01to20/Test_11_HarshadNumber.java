7456de6e9f34e4eb396ef534c7af254d43943684
sangwoo lee <lswoo0705@gmail.com>
2023-11-01 10:25:34 +0900

    프로그래머스 1-11

---
+++ src/main/java/algorithm/programmers1/test_01to20/Test_11_HarshadNumber.java
@@ -0,0 +1,16 @@
+package algorithm.programmers1.test_01to20;
+
+public class Test_11_HarshadNumber {
+    public boolean solution(int x) {
+        int sumX = 0;
+        String strX = Integer.toString(x);
+        for (int i = 0; i < strX.length(); i++) {
+            sumX += Integer.parseInt(strX.substring(i, i + 1));
+        }
+        if (x % sumX == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
