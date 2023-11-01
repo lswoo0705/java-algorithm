5a29d2b6e21550ad08b560eb5d105db7ae8175d5
sangwoo lee <lswoo0705@gmail.com>
2023-11-01 12:25:16 +0900

    프로그래머스 1-13

---
+++ src/main/java/algorithm/programmers1/test_01to20/Test_13_CollatzGuesses.java
@@ -0,0 +1,21 @@
+package algorithm.programmers1.test_01to20;
+
+public class Test_13_CollatzGuesses {
+    public int solution(int num) {
+        int count = 0;
+        Long longNum = (long) num;
+        while (longNum != 1) {
+            if (longNum % 2 == 0) {
+                longNum = longNum / 2;
+                count++;
+            } else {
+                longNum = longNum * 3 + 1;
+                count++;
+            }
+        }
+        if (count >= 500) {
+            count = -1;
+        }
+        return count;
+    }
+}
