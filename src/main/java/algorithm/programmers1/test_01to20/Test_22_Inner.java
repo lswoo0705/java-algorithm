f694f771308ead26346fcd81274614090d7135f9
sangwoo lee <lswoo0705@gmail.com>
2023-11-08 12:24:08 +0900

    프로그래머스 1-22

---
+++ src/main/java/algorithm/programmers1/test_01to20/Test_22_Inner.java
@@ -0,0 +1,11 @@
+package algorithm.programmers1.test_01to20;
+
+public class Test_22_Inner {
+    public int solution(int[] a, int[] b) {
+        int answer = 0;
+        for (int i = 0; i < a.length; i++) {
+            answer += a[i] * b[i];
+        }
+        return answer;
+    }
+}
