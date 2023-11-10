4760791d7c3f0bce570c70541ea6c4ec07d49781
sangwoo lee <lswoo0705@gmail.com>
2023-11-10 11:46:48 +0900

    프로그래머스 1-29

---
+++ src/main/java/algorithm/programmers1/test_21to40/Test_29_GreatestComDivisorAndLeastComMultiple.java
@@ -0,0 +1,25 @@
+package algorithm.programmers1.test_21to40;
+
+public class Test_29_GreatestComDivisorAndLeastComMultiple {
+    public int[] solution(int n, int m) {
+        int[] answer = new int[2];
+        int lowNum = 0;
+        int gCD = 0;
+        int lCM = 0;
+        // 최대공약수를 구하기 위해 n, m 중 작은 수 찾기
+        lowNum = Math.min(n, m);
+        // 최대공약수 구하기
+        for (int i = 1; i <= lowNum; i++) {
+            if (n % i == 0 && m % i == 0) {
+                gCD = i;
+            }
+        }
+        // 최소공배수 구하기
+        lCM = n * m / gCD;
+
+        answer[0] = gCD;
+        answer[1] = lCM;
+
+        return answer;
+    }
+}
