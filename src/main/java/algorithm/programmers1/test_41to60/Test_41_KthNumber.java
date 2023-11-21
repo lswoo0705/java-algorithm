b5ab7333f7a0742e02c3780eacf776667a1c8b0c
sangwoo lee <lswoo0705@gmail.com>
2023-11-21 10:34:23 +0900

    프로그래머스 1-41

---
+++ src/main/java/algorithm/programmers1/test_41to60/Test_41_KthNumber.java
@@ -0,0 +1,23 @@
+package algorithm.programmers1.test_41to60;
+
+import java.util.Arrays;
+
+public class Test_41_KthNumber {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        for (int i = 0; i < commands.length; i++) {
+            int[] arr = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
+            Arrays.sort(arr);
+            answer[i] = arr[commands[i][2] - 1];
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Test_41_KthNumber sol = new Test_41_KthNumber();
+        int[] arr = {1, 5, 2, 6, 3, 7, 4};
+        int[][] commands = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
+        System.out.println(Arrays.toString(sol.solution(arr, commands)));
+    }
+}
