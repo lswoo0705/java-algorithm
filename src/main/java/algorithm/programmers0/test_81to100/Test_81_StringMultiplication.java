308a0dad8a4eada59e53b76d202bcb7cfd31e6b4
sangwoo lee <lswoo0705@gmail.com>
2024-04-01 23:52:14 +0900

    프로그래머스 0-81

---
+++ src/main/java/algorithm/programmers0/test_81to100/Test_81_StringMultiplication.java
@@ -0,0 +1,7 @@
+package algorithm.programmers0.test_81to100;
+
+public class Test_81_StringMultiplication {
+    public String solution(String my_string, int k) {
+        return my_string.repeat(k);
+    }
+}
