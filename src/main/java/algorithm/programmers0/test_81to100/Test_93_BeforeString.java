7048f79ff1a41333fa3c1248894caf5cc1833ff1
sangwoo lee <lswoo0705@gmail.com>
2024-04-04 20:31:27 +0900

    프로그래머스 0-93

---
+++ src/main/java/algorithm/programmers0/test_81to100/Test_93_BeforeString.java
@@ -0,0 +1,7 @@
+package algorithm.programmers0.test_81to100;
+
+public class Test_93_BeforeString {
+    public String solution(String my_string, int n) {
+        return my_string.substring(0, n);
+    }
+}
