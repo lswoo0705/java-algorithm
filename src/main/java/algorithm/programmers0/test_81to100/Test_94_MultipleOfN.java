698a76d9dfdd8c6a8d6f0ed8271a3eed5c21b66c
sangwoo lee <lswoo0705@gmail.com>
2024-04-05 19:06:56 +0900

    프로그래머스 0-94

---
+++ src/main/java/algorithm/programmers0/test_81to100/Test_94_MultipleOfN.java
@@ -0,0 +1,13 @@
+package algorithm.programmers0.test_81to100;
+
+public class Test_94_MultipleOfN {
+    public int solution(int num, int n) {
+//        if (num % n == 0) {
+//            return 1;
+//        } else {
+//            return 0;
+//        }
+
+        return num % n == 0 ? 1 : 0;
+    }
+}
