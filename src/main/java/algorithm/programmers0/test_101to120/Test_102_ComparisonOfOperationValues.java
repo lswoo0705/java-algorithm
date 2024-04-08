9f20833fb8e34a4e03b1b0d9018f8d8fde59a476
sangwoo lee <lswoo0705@gmail.com>
2024-04-09 00:15:29 +0900

    프로그래머스 0-102

---
+++ src/main/java/algorithm/programmers0/test_101to120/Test_102_ComparisonOfOperationValues.java
@@ -0,0 +1,7 @@
+package algorithm.programmers0.test_101to120;
+
+public class Test_102_ComparisonOfOperationValues {
+    public int solution(int a, int b) {
+        return Math.max(Integer.parseInt(Integer.toString(a) + Integer.toString(b)), 2 * a * b);
+    }
+}
