b1ed71bdbd29600987a2f27565e4696f1bcdf53f
sangwoo lee <lswoo0705@gmail.com>
2024-04-12 19:55:38 +0900

    프로그래머스 0-108

---
+++ src/main/java/algorithm/programmers0/test_101to120/Test_108_TailString.java
@@ -0,0 +1,17 @@
+package algorithm.programmers0.test_101to120;
+
+import java.util.Arrays;
+
+public class Test_108_TailString {
+    public String solution(String[] str_list, String ex) {
+        StringBuilder answer = new StringBuilder();
+
+        for (String s : str_list) {
+            if (!s.contains(ex)) {
+                answer.append(s);
+            }
+        }
+        
+        return answer.toString();
+    }
+}
