bc3c78df68dc03067919c3e3014351f0cc8b2fe0
sangwoo lee <lswoo0705@gmail.com>
2024-04-15 22:26:47 +0900

    프로그래머스 0-114

---
+++ src/main/java/algorithm/programmers0/test_101to120/Test_114_5thFromTheBack.java
@@ -0,0 +1,14 @@
+package algorithm.programmers0.test_101to120;
+
+import java.util.Arrays;
+
+public class Test_114_5thFromTheBack {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[5];
+        Arrays.sort(num_list);
+
+        System.arraycopy(num_list, 0, answer, 0, 5);
+
+        return answer;
+    }
+}
