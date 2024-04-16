2bbb17cad75cc6da1e99376bbb8feabf60e0fdb1
sangwoo lee <lswoo0705@gmail.com>
2024-04-16 18:54:05 +0900

    프로그래머스 0-117

---
+++ src/main/java/algorithm/programmers0/test_101to120/Test_117_rnyString.java
@@ -0,0 +1,7 @@
+package algorithm.programmers0.test_101to120;
+
+public class Test_117_rnyString {
+    public String solution(String rny_string) {
+        return rny_string.replace("m", "rn");
+    }
+}
