0189c86569fa33d6c091e5aca5ba0eba908a12bc
sangwoo lee <lswoo0705@gmail.com>
2024-04-24 19:10:14 +0900

    프로그래머스 2-22

---
+++ src/main/java/algorithm/programmers2/test_21to40/Test_22_MatrixMultiplication.java
@@ -0,0 +1,17 @@
+package algorithm.programmers2.test_21to40;
+
+public class Test_22_MatrixMultiplication {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = new int[arr1.length][arr2[0].length];
+
+        for(int i = 0; i < answer.length; i++){
+            for(int j = 0; j < answer[i].length; j++){
+                for(int k = 0; k < arr1[0].length; k++){
+                    answer[i][j] += arr1[i][k]*arr2[k][j];
+                }
+            }
+        }
+
+        return answer;
+    }
+}
