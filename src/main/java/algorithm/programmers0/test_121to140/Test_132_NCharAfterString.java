3adcc46c93d8ee3e54d8892662c5c08f7da7fa39
sangwoo lee <lswoo0705@gmail.com>
2024-05-08 18:53:31 +0900

    프로그래머스 0-132

---
+++ src/main/java/algorithm/programmers0/test_121to140/Test_132_NCharAfterString.java
@@ -0,0 +1,7 @@
+package algorithm.programmers0.test_121to140;
+
+public class Test_132_NCharAfterString {
+    public String solution(String my_string, int n) {
+        return my_string.substring(my_string.length() - n);
+    }
+}
