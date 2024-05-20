784dd26f6e915a8650de5344f86d7a024e107b12
sangwoo lee <lswoo0705@gmail.com>
2024-05-21 00:47:11 +0900

    프로그래머스 0-154

---
+++ src/main/java/algorithm/programmers0/test_141to160/Test_154_PrintingAddition.java
@@ -0,0 +1,15 @@
+package algorithm.programmers0.test_141to160;
+
+import java.util.Scanner;
+
+public class Test_154_PrintingAddition {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int c = a + b;
+
+        System.out.println(a + " + " + b + " = " + c);
+    }
+}
