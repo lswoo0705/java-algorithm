0ea14b481bdd1f902bf86cc7d087a1913aab2405
sangwoo lee <lswoo0705@gmail.com>
2024-05-21 00:39:08 +0900

    프로그래머스 0-152

---
+++ src/main/java/algorithm/programmers0/test_141to160/Test_152_DiceGameTwo.java
@@ -0,0 +1,18 @@
+package algorithm.programmers0.test_141to160;
+
+// 주사위 게임 2
+public class Test_152_DiceGameTwo {
+    public int solution(int a, int b, int c) {
+        int answer = 0;
+
+        if (a != b && a != c && b != c) {
+            answer = a + b + c;
+        } else if (a == b && a == c) {
+            answer = (a + b + c) * (a * a + b * b + c * c) * (a * a * a + b * b * b + c * c * c);
+        } else {
+            answer = (a + b + c) * (a * a + b * b + c * c);
+        }
+
+        return answer;
+    }
+}
