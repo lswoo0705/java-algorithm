95d919dee8d6b17c74e0733a25bdd0caa5be3614
sangwoo lee <lswoo0705@gmail.com>
2024-05-21 20:05:38 +0900

    프로그래머스 0-156

---
+++ src/main/java/algorithm/programmers0/test_141to160/Test_156_StringArrToString.java
@@ -0,0 +1,13 @@
+package algorithm.programmers0.test_141to160;
+
+public class Test_156_StringArrToString {
+    public String solution(String[] arr) {
+        StringBuilder answer = new StringBuilder();
+
+        for (String s : arr) {
+            answer.append(s);
+        }
+
+        return answer.toString();
+    }
+}
