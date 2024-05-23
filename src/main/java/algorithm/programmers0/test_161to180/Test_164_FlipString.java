5f30ae06274677776203b1ca83c72b2ba430613e
sangwoo lee <lswoo0705@gmail.com>
2024-05-23 20:16:12 +0900

    프로그래머스 0-164

---
+++ src/main/java/algorithm/programmers0/test_161to180/Test_164_FlipString.java
@@ -0,0 +1,17 @@
+package algorithm.programmers0.test_161to180;
+
+public class Test_164_FlipString {
+    public String solution(String my_string, int s, int e) {
+        StringBuilder answer = new StringBuilder();
+
+        answer.append(my_string, 0, s);
+
+        for (int i = e; i >= s; i--) {
+            answer.append(my_string.charAt(i));
+        }
+
+        answer.append(my_string.substring(e + 1));
+
+        return answer.toString();
+    }
+}
