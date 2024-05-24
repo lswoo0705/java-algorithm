f9bb1670be2714bd377a2a54138d6865a79267bb
sangwoo lee <lswoo0705@gmail.com>
2024-05-24 19:40:27 +0900

    프로그래머스 0-167

---
+++ src/main/java/algorithm/programmers0/test_161to180/Test_167_FindSubstring.java
@@ -0,0 +1,9 @@
+package algorithm.programmers0.test_161to180;
+
+public class Test_167_FindSubstring {
+    public String solution(String myString, String pat) {
+        int index = myString.lastIndexOf(pat) + pat.length();
+        
+        return myString.substring(0, index);
+    }
+}
