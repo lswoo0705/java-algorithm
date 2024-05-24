1c9ab35290a35dcda38e995f6694cf6ee87d5db7
sangwoo lee <lswoo0705@gmail.com>
2024-05-24 19:26:58 +0900

    프로그래머스 0-165

---
+++ src/main/java/algorithm/programmers0/test_161to180/Test_165_CreatingArr5.java
@@ -0,0 +1,38 @@
+package algorithm.programmers0.test_161to180;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test_165_CreatingArr5 {
+    public int[] solution(String[] intStrs, int k, int s, int l) {
+//        List<Integer> numList = new ArrayList<>();
+//
+//        for (String intStr : intStrs) {
+//            int num = Integer.parseInt(intStr.substring(s, s + l));
+//
+//            if (num > k) {
+//                numList.add(num);
+//            }
+//        }
+//
+//        int[] answer = new int[numList.size()];
+//
+//        for (int i = 0; i < numList.size(); i++) {
+//            answer[i] = numList.get(i);
+//        }
+//
+//        return answer;
+
+        List<Integer> numList = new ArrayList<>();
+
+        for (String intStr : intStrs) {
+            int num = Integer.parseInt(intStr.substring(s, s + l));
+
+            if (num > k) {
+                numList.add(num);
+            }
+        }
+
+        return numList.stream().mapToInt(i -> i).toArray();
+    }
+}
