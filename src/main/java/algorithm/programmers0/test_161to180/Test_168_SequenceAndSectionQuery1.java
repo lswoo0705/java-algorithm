f69c70f450d712c1ac1b0c65a6df25e28a4b6156
sangwoo lee <lswoo0705@gmail.com>
2024-06-03 21:39:15 +0900

    프로그래머스 0-168

---
+++ src/main/java/algorithm/programmers0/test_161to180/Test_168_SequenceAndSectionQuery1.java
@@ -0,0 +1,16 @@
+package algorithm.programmers0.test_161to180;
+
+public class Test_168_SequenceAndSectionQuery1 {
+    public int[] solution(int[] arr, int[][] queries) {
+        for (int[] query : queries) {
+            int start = query[0];
+            int end = query[1];
+
+            for (int i = start; i <= end; i++) {
+                arr[i]++;
+            }
+        }
+
+        return arr;
+    }
+}
