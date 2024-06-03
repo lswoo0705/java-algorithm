3b947349f9897d193bb61dc511c5ce6c59d94c95
sangwoo lee <lswoo0705@gmail.com>
2024-06-03 21:30:08 +0900

    프로그래머스 2-28

---
+++ src/main/java/algorithm/programmers2/test_21to40/Test_28_Process.java
@@ -0,0 +1,37 @@
+package algorithm.programmers2.test_21to40;
+
+import java.util.*;
+
+public class Test_28_Process {
+    public int solution(int[] priorities, int location) {
+        int answer = 0;
+
+        Queue<Integer> queue = new LinkedList<>();
+        for (int priority : priorities) {
+            queue.offer(priority);
+        }
+
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+            boolean isMax = true;
+
+            for (int priority : queue) {
+                if (priority > current) {
+                    isMax = false;
+                    break;
+                }
+            }
+
+            if (isMax) {
+                answer++;
+                if (location == 0) break;
+            } else {
+                queue.offer(current);
+            }
+
+            location = (location - 1 + queue.size()) % queue.size();
+        }
+
+        return answer;
+    }
+}
