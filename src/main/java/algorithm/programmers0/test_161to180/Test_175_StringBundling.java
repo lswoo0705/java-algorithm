06cc8c44a891a41e1c6ec4b2ea6a265be0fe07a5
sangwoo lee <lswoo0705@gmail.com>
2024-06-13 22:08:02 +0900

    프로그래머스 0-175

---
+++ src/main/java/algorithm/programmers0/test_161to180/Test_175_StringBundling.java
@@ -0,0 +1,22 @@
+package algorithm.programmers0.test_161to180;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Test_175_StringBundling {
+    public int solution(String[] strArr) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int answer = 0;
+
+        for (String str : strArr) {
+            int length = str.length();
+            map.put(length, map.getOrDefault(length, 0) + 1);
+        }
+
+        for (int n : map.values()) {
+            answer = Math.max(answer, n);
+        }
+
+        return answer;
+    }
+}
