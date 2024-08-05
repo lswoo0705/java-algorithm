55bb2e96f9a551c75731650196dbcdc2ba77e797
sangwoo lee <lswoo0705@gmail.com>
2024-08-05 11:00:28 +0900

    프로그래머스 0-177

---
+++ src/main/java/algorithm/programmers0/test_161to180/Test_177_CuttingList.java
@@ -0,0 +1,41 @@
+package algorithm.programmers0.test_161to180;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// 리스트 자르기
+public class Test_177_CuttingList {
+    public int[] solution(int n, int[] slicer, int[] num_list) {
+        List<Integer> result = new ArrayList<>();
+
+        switch (n) {
+            case 1 -> {
+                for (int i = 0; i <= slicer[1]; i++) {
+                    result.add(num_list[i]);
+                }
+            }
+            case 2 -> {
+                for (int i = slicer[0]; i < num_list.length; i++) {
+                    result.add(num_list[i]);
+                }
+            }
+            case 3 -> {
+                for (int i = slicer[0]; i <= slicer[1]; i++) {
+                    result.add(num_list[i]);
+                }
+            }
+            case 4 -> {
+                for (int i = slicer[0]; i <= slicer[1]; i += slicer[2]) {
+                    result.add(num_list[i]);
+                }
+            }
+        }
+
+        int[] answer = new int[result.size()];
+        for (int i = 0; i < result.size(); i++) {
+            answer[i] = result.get(i);
+        }
+
+        return answer;
+    }
+}
