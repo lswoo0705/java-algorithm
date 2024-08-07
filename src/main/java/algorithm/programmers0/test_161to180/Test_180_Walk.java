d1b4e0e220a0441128fe9f4bf95ecfd307b605c4
sangwoo lee <lswoo0705@gmail.com>
2024-08-07 10:17:16 +0900

    프로그래머스 0-180

---
+++ src/main/java/algorithm/programmers0/test_161to180/Test_180_Walk.java
@@ -0,0 +1,29 @@
+package algorithm.programmers0.test_161to180;
+
+// [PCCE 기출문제] 5번 / 산책
+public class Test_180_Walk {
+    public int[] solution(String route) {
+        int east = 0;
+        int north = 0;
+        int[] answer = new int[2];
+        for (int i = 0; i < route.length(); i++) {
+            switch (route.charAt(i)) {
+                case 'N':
+                    north++;
+                    break;
+                case 'S':
+                    north--;
+                    break;
+                case 'E':
+                    east++;
+                    break;
+                case 'W':
+                    east--;
+                    break;
+            }
+        }
+        answer[0] = east;
+        answer[1] = north;
+        return answer;
+    }
+}
