10d1cfc3e341f31666f9627c203400c5e04badd8
sangwoo lee <lswoo0705@gmail.com>
2024-08-22 12:14:30 +0900

    프로그래머스 0-184

---
+++ src/main/java/algorithm/programmers0/test_181to200/Test_184_OutputSpecialChar.java
@@ -0,0 +1,8 @@
+package algorithm.programmers0.test_181to200;
+import java.util.Scanner;
+
+public class Test_184_OutputSpecialChar {
+    public static void main(String[] args) {
+        System.out.println("!@#$%^&*(\\\'\"<>?:;");
+    }
+}
