f18f711fc03abf9396d72ca83c83494a3c81b85b
sangwoo lee <lswoo0705@gmail.com>
2024-08-29 11:36:39 +0900

    백준 1-01

---
+++ src/main/java/baekjoon/level1/Main.java
@@ -0,0 +1,13 @@
+package baekjoon.level1;
+
+public class Main {
+
+    public static void solution() {
+        //솔루션 코드 작성
+    }
+
+    public static void main(String[] args) {
+        solution();
+    }
+
+}
---
+++ src/main/java/baekjoon/level1/Test_01_HelloWorld.java
@@ -0,0 +1,13 @@
+package baekjoon.level1;
+
+public class Test_01_HelloWorld {
+
+    public static void solution() {
+        System.out.println("Hello World!");
+    }
+
+    public static void main(String[] args) {
+        solution();
+    }
+
+}
