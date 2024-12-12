d60864b6832712a63b68fb1c9d845f1739435fa9
sangwoo lee <lswoo0705@gmail.com>
2024-12-12 11:19:51 +0900

    프로그래머스 0-193

---
+++ src/main/java/programmers/programmers0/test_181to200/Test_193_Saving.java
@@ -0,0 +1,25 @@
+package programmers.programmers0.test_181to200;
+
+import java.util.Scanner;
+
+public class Test_193_Saving {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int start = sc.nextInt();
+        int before = sc.nextInt();
+        int after = sc.nextInt();
+
+        int money = start;
+        int month = 1;
+        while (money < 70) {
+            money += before;
+            month++;
+        }
+        while (money < 100) {
+            money += after;
+            month++;
+        }
+
+        System.out.println(month);
+    }
+}
