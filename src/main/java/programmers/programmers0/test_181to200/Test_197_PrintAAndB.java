c46d2735dbf2dd5601a04ec9493b3e4e0e08a04e
sangwoo lee <lswoo0705@gmail.com>
2025-01-03 19:28:00 +0900

    프로그래머스 0-197

---
+++ src/main/java/programmers/programmers0/test_181to200/Test_197_PrintAAndB.java
@@ -0,0 +1,14 @@
+package programmers.programmers0.test_181to200;
+
+import java.util.Scanner;
+
+public class Test_197_PrintAAndB {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
