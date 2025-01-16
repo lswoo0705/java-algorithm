d753bfaae0016610acc7bf0c3e1c5e0633fffb72
sangwoo lee <lswoo0705@gmail.com>
2025-01-16 10:30:16 +0900

    프로그래머스 0-213

---
+++ src/main/java/programmers/programmers0/test_201to220/Test_213_PrintString.java
@@ -0,0 +1,10 @@
+package programmers.programmers0.test_201to220;
+
+public class Test_213_PrintString {
+    public static void main(String[] args) {
+        String message = "Let's go!";
+
+        System.out.println("3\n2\n1");
+        System.out.println(message);
+    }
+}
