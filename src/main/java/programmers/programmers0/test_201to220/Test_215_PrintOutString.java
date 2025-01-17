4c6c5b76aed330b4d543980240d21586ce2162dd
sangwoo lee <lswoo0705@gmail.com>
2025-01-17 11:59:19 +0900

    프로그래머스 0-215

---
+++ src/main/java/programmers/programmers0/test_201to220/Test_215_PrintOutString.java
@@ -0,0 +1,11 @@
+package programmers.programmers0.test_201to220;
+
+import java.util.Scanner;
+
+public class Test_215_PrintOutString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        System.out.println(a);
+    }
+}
