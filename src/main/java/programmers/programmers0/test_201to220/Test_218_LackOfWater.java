174b69b9a518de50dff697111393bfea49770690
sangwoo lee <lswoo0705@gmail.com>
2025-01-20 18:16:52 +0900

    프로그래머스 0-218

---
+++ src/main/java/programmers/programmers0/test_201to220/Test_218_LackOfWater.java
@@ -0,0 +1,15 @@
+package programmers.programmers0.test_201to220;
+
+public class Test_218_LackOfWater {
+    public int solution(int storage, int usage, int[] change) {
+        int total_usage = 0;
+        for(int i=0; i<change.length; i++){
+            usage = usage + (usage * change[i] / 100);
+            total_usage += usage;
+            if(total_usage > storage){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
