0a5a2fe549be1c623aa76307098dfeb172bf73fa
sangwoo lee <lswoo0705@gmail.com>
2025-04-08 22:46:11 +0900

    프로그래머스 2-80-2

--- src/main/java/programmers/programmers2/test_61to80/Test_80_TowerOfHanoi.java
+++ src/main/java/programmers/programmers2/test_61to80/Test_80_TowerOfHanoi.java
@@ -7,8 +7,14 @@ public class Test_80_TowerOfHanoi {
     List<int[]> moves = new ArrayList<>();
 
     public int[][] solution(int n) {
+        // n개의 원판을 from 기둥에서 to 기둥으로 옮기기 위해
+        // n-1개의 원판을 from → via로 옮긴다.
+        // 가장 큰 원판 1개를 from → to로 옮긴다.
+        // n-1개의 원판을 via → to로 옮긴다.
+
         hanoi(n, 1, 3, 2);
 
+        // 리스트를 배열로
         int[][] answer = new int[moves.size()][2];
         for (int i = 0; i < moves.size(); i++) {
             answer[i] = moves.get(i);
@@ -23,8 +29,8 @@ public class Test_80_TowerOfHanoi {
             return;
         }
 
-        hanoi(n - 1, from, via, to);
-        moves.add(new int[]{from, to});
-        hanoi(n - 1, via, to, from);
+        hanoi(n - 1, from, via, to); // 1단계
+        moves.add(new int[]{from, to}); // 2단계
+        hanoi(n - 1, via, to, from); // 3단계
     }
 }
