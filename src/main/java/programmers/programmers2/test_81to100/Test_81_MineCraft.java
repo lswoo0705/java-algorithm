180c056bc874ae43fffd4178b1425b96e62ab566
sangwoo lee <lswoo0705@gmail.com>
2025-04-10 20:06:58 +0900

    프로그래머스 2-81-1

---
+++ src/main/java/programmers/programmers2/test_81to100/Test_81_MineCraft.java
@@ -0,0 +1,19 @@
+package programmers.programmers2.test_81to100;
+
+public class Test_81_MineCraft {
+    public int solution(int[] picks, String[] minerals) {
+        // 무조건 광물은 5개마다 하나의 곡괭이를 사용한다.
+        // minerals를 미리 5개 씩 나눌 수 있다.
+        // 각 묶음별로 피로도를 구해서 피로도가 높은거는 좋은 곡괭이로?
+
+        int answer = 0;
+
+        // 1.리스트를 만들어서 광물 5개 씩 넣기
+
+        // 2.묶음별로 피로도를 계산
+
+        // 3.곡괭이를 피로도가 높은 순으로 할당해서 계산
+
+        return answer;
+    }
+}
