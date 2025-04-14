7b672acd9673cdbfd120ccfd15ad155b49afb2e3
sangwoo lee <lswoo0705@gmail.com>
2025-04-14 20:42:39 +0900

    프로그래머스 2-82-1

---
+++ src/main/java/programmers/programmers2/test_81to100/Test_82_StringCompression.java
@@ -0,0 +1,14 @@
+package programmers.programmers2.test_81to100;
+
+public class Test_82_StringCompression {
+    public int solution(String s) {
+        int answer = 0;
+
+        // 1.압축 단위를 1부터 반복하면서 압축
+        // 2.substring으로 자르기
+        // 3.StringBuilder로 문자 연결
+        // 4.길이 비교해서 정답을 반환
+
+        return answer;
+    }
+}
