9e09952c68070e68332d6d570751c4697da20207
sangwoo lee <lswoo0705@gmail.com>
2025-04-16 12:44:56 +0900

    프로그래머스 2-83-1

---
+++ src/main/java/programmers/programmers2/test_81to100/Test_83_NormalSquare.java
@@ -0,0 +1,12 @@
+package programmers.programmers2.test_81to100;
+
+public class Test_83_NormalSquare { // 멀쩡한 사각형
+    public long solution(int w, int h) {
+        // 1.큰 직사각형의 꼭지점을 잇는 대각선으로 자르는 것이므로 최소크기의 대각선 크기를 찾는다.
+        // ex) 가로4 * 세로6 크기의 직사각형이 주어진다면 최소 크기는 가로2 * 세로3
+        // 2. 가로2 * 세로3 에서의 사용할 수 없는 정사각형의 개수를 구하고
+        // 3. 가로 또는 세로의 크기만큼 배수를 적용하면 총 사용할 수 없는 정사각형의 수를 구할 수 있다.
+        long answer = 1;
+        return answer;
+    }
+}
