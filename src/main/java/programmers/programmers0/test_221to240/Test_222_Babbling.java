385a8df7011fd1797fd9611217c26431f9a833b3
sangwoo lee <lswoo0705@gmail.com>
2025-04-17 20:17:16 +0900

    프로그래머스 0-222-1

---
+++ src/main/java/programmers/programmers0/test_221to240/Test_222_Babbling.java
@@ -0,0 +1,12 @@
+package programmers.programmers0.test_221to240;
+
+public class Test_222_Babbling { // 옹알이(1)
+    public int solution(String[] babbling) {
+        int answer = 0;
+        // aya, ye, woo, ma 문자열들을 고정으로
+        // babbling 배열을 돌아가며 스트링빌더로 빼냄
+        // 옹알이 문자열이 있을 경우 그만큼 잘라내고
+        // 남는 문자가 없을 경우 answer++
+        return answer;
+    }
+}
