8d6e811b1938cd7dc7a907aa473ee666a2c43526
sangwoo lee <lswoo0705@gmail.com>
2025-04-22 19:56:44 +0900

    프로그래머스 2-84-1

---
+++ src/main/java/programmers/programmers2/test_81to100/Test_84_HailSequenceDefiniteIntegral.java
@@ -0,0 +1,11 @@
+package programmers.programmers2.test_81to100;
+
+public class Test_84_HailSequenceDefiniteIntegral { // 우박수열 정적분
+    public double[] solution(int k, int[][] ranges) {
+        double[] answer = {};
+        // 1.콜라츠 수열을 구하기
+        // 2.구간별 면적을 미리 구해놓기
+        // 3.각 구간별 면적 값을 구하기
+        return answer;
+    }
+}
