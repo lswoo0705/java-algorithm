180904b864dbec80d848d95cf856f26421128c9e
sangwoo lee <lswoo0705@gmail.com>
2025-04-25 20:07:00 +0900

    프로그래머스 2-85-2

--- src/main/java/programmers/programmers2/test_81to100/Test_85_MakingAPoint.java
+++ src/main/java/programmers/programmers2/test_81to100/Test_85_MakingAPoint.java
@@ -2,10 +2,22 @@ package programmers.programmers2.test_81to100;
 
 public class Test_85_MakingAPoint { // 점 찍기
     public long solution(int k, int d) {
-        // (0, 0)은 항상 찍혀있으므로 answer = 1 부터 시작
-        // 2중 for문으로 a와 b가 증가할 때 d 이하인 좌표인 경우에만 추가
-        // d 이하의 거리인 좌표 확인은 (d의 제곱) >= (x의 제곱) + (y의 제곱)으로 확인
         long answer = 0;
+        int distance = d * d;
+
+        // 2중 for문으로 a와 b가 증가할 때 d 이하인 좌표인 경우에만 추가
+        for (int x = 0; x <= d; x++) {
+            for (int y = 0; y <= d; y++) {
+                // d 이하의 거리인 좌표 확인은 (d의 제곱) >= (x의 제곱) + (y의 제곱)으로 확인
+                if ((x * k) * (x * k) + (y * k) * (y * k) <= distance) {
+                    answer++;
+                }
+            }
+        }
+
         return answer;
+
+        // ----------- 정확성 : 6.3 / 100.0 -> 실패(시간초과) -----------
+        // 시간복잡도 개선 필요!!
     }
 }
