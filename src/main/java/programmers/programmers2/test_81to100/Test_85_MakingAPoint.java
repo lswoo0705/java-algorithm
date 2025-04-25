586eaee7d72a020ba2fa27a0d7a679fb2e997510
sangwoo lee <lswoo0705@gmail.com>
2025-04-25 19:53:10 +0900

    프로그래머스 2-85-1

---
+++ src/main/java/programmers/programmers2/test_81to100/Test_85_MakingAPoint.java
@@ -0,0 +1,11 @@
+package programmers.programmers2.test_81to100;
+
+public class Test_85_MakingAPoint { // 점 찍기
+    public long solution(int k, int d) {
+        // (0, 0)은 항상 찍혀있으므로 answer = 1 부터 시작
+        // 2중 for문으로 a와 b가 증가할 때 d 이하인 좌표인 경우에만 추가
+        // d 이하의 거리인 좌표 확인은 (d의 제곱) >= (x의 제곱) + (y의 제곱)으로 확인
+        long answer = 0;
+        return answer;
+    }
+}
