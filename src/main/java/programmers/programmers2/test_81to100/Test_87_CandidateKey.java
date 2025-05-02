07adb0917498235ddc7baab7c2b27adba417e191
sangwoo lee <lswoo0705@gmail.com>
2025-05-02 12:45:59 +0900

    프로그래머스 2-87-1

---
+++ src/main/java/programmers/programmers2/test_81to100/Test_87_CandidateKey.java
@@ -0,0 +1,11 @@
+package programmers.programmers2.test_81to100;
+
+public class Test_87_CandidateKey { // 후보키
+    public int solution(String[][] relation) {
+        // 1. 컬럼들의 모든 조합을 만든다.
+        // 2. 유일성을 확인하기 위해 중복을 검사해야하므로 Set을 사용한다.
+        // 3. 다른 후보키들과 비교해서 최소성 확인.
+        int answer = 0;
+        return answer;
+    }
+}
