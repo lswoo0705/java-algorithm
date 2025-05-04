bdd197da24fab18299843b1976ab9c1551d41cc6
sangwoo lee <lswoo0705@gmail.com>
2025-05-04 15:34:48 +0900

    백준 3-1-2

--- src/main/java/baekjoon/level3/Test_01_APlusBMinus4.java
+++ src/main/java/baekjoon/level3/Test_01_APlusBMinus4.java
@@ -10,11 +10,28 @@ public class Test_01_APlusBMinus4 {
         return Integer.parseInt(s);
     }
 
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+//
+//        while (br.readLine() != null) {
+//            int a = stoi(st.nextToken());
+//            int b = stoi(st.nextToken());
+//
+//            System.out.println(a + b);
+//        }
+//    }
+
+    // 런타임 에러 : 반복문이 반복될 때마다 StringTokenizer 호출하므로
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        while (br.readLine() != null) {
+        // 입력이 있을 때 반복
+        String line;
+        while ((line = br.readLine()) != null) {
+            StringTokenizer st = new StringTokenizer(line);
+
             int a = stoi(st.nextToken());
             int b = stoi(st.nextToken());
 
