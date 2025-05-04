db26f6685d0f0d8a7718def753ddcc63d51cd61d
sangwoo lee <lswoo0705@gmail.com>
2025-05-04 14:59:33 +0900

    백준 1-07

---
+++ src/main/java/baekjoon/level1/Test_07_QuestionMark.java
@@ -0,0 +1,15 @@
+package baekjoon.level1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Test_07_QuestionMark {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String id = br.readLine();
+
+        System.out.println(id + "??!");
+    }
+}
