5483e768c45fa8c55f80639f8f005182e965b74e
sangwoo lee <lswoo0705@gmail.com>
2025-05-04 15:23:33 +0900

    백준 Main Class

--- src/main/java/baekjoon/Main.java
+++ src/main/java/baekjoon/Main.java
@@ -1,13 +1,24 @@
 package baekjoon;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main {
+    static int N;
 
-    public static void solution() {
-        //솔루션 코드 작성
+    // 문자열을 정수로
+    static int stoi(String s) {
+        return Integer.parseInt(s);
     }
 
-    public static void main(String[] args) {
-        solution();
+    public static void main(String[] args) throws IOException {
+        // 문제에서 주어진 문자열
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // 문제에서 주어진 문자열을 토큰으로 나눈 문자열
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        // 첫 토큰의 정수형
+        N = stoi(st.nextToken());
     }
-
 }
