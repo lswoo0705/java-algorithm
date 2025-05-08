5ea21c27f24ff24466d99a6ca11a6eeea9b9f0a0
sangwoo lee <lswoo0705@gmail.com>
2025-05-08 11:52:18 +0900

    백준 1-13

---
+++ src/main/java/baekjoon/level1/Test_13_Dog.java
@@ -0,0 +1,13 @@
+package baekjoon.level1;
+
+import java.io.IOException;
+
+public class Test_13_Dog {
+    public static void main(String[] args) throws IOException {
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
