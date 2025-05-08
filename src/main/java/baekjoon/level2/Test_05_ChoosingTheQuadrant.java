904afa6ae6618d60592875ad3478c32469cca53d
sangwoo lee <lswoo0705@gmail.com>
2025-05-08 12:35:59 +0900

    백준 2-5

---
+++ src/main/java/baekjoon/level2/Test_05_ChoosingTheQuadrant.java
@@ -0,0 +1,28 @@
+package baekjoon.level2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Test_05_ChoosingTheQuadrant {
+    static int stoi(String s) {
+        return Integer.parseInt(s);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int x = stoi(br.readLine());
+        int y = stoi(br.readLine());
+
+        if (x > 0 && y > 0) {
+            System.out.println(1);
+        } else if (x < 0 && y > 0) {
+            System.out.println(2);
+        } else if (x < 0 && y < 0) {
+            System.out.println(3);
+        } else {
+            System.out.println(4);
+        }
+    }
+}
