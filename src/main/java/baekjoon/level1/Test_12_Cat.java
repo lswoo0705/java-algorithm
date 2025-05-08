293e4106c6991d1072ab0d39fa34fbde5cc069d4
sangwoo lee <lswoo0705@gmail.com>
2025-05-08 11:52:11 +0900

    백준 1-12

---
+++ src/main/java/baekjoon/level1/Test_12_Cat.java
@@ -0,0 +1,12 @@
+package baekjoon.level1;
+
+import java.io.IOException;
+
+public class Test_12_Cat {
+    public static void main(String[] args) throws IOException {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
