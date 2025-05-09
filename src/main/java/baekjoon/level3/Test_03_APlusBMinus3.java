9d65bf0fd14800262af35e7544e65cbe017ee5ca
sangwoo lee <lswoo0705@gmail.com>
2025-05-09 20:12:13 +0900

    백준 3-3

---
+++ src/main/java/baekjoon/level3/Test_03_APlusBMinus3.java
@@ -0,0 +1,23 @@
+package baekjoon.level3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Test_03_APlusBMinus3 {
+    static int stoi(String s) {
+        return Integer.parseInt(s);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = stoi(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            System.out.println(stoi(st.nextToken()) + stoi(st.nextToken()));
+        }
+    }
+}
