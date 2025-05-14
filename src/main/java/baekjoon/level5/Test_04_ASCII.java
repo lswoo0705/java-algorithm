c6e9f17f8e8a19da38fa792d3c097c4038d09e3b
sangwoo lee <lswoo0705@gmail.com>
2025-05-14 20:06:48 +0900

    백준 5-4

---
+++ src/main/java/baekjoon/level5/Test_04_ASCII.java
@@ -0,0 +1,11 @@
+package baekjoon.level5;
+
+import java.io.*;
+
+public class Test_04_ASCII {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println((int) br.readLine().charAt(0));
+    }
+}
