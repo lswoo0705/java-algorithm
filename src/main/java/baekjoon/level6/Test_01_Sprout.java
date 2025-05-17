33152a44cedb4000c7e9f8a2aecccd34852236aa
sangwoo lee <lswoo0705@gmail.com>
2025-05-17 13:33:24 +0900

    백준 6-1

---
+++ src/main/java/baekjoon/level6/Test_01_Sprout.java
@@ -0,0 +1,18 @@
+package baekjoon.level6;
+
+import java.io.*;
+public class Test_01_Sprout {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write("         ,r'\"7\n");
+        bw.write("r`-_   ,'  ,/\n");
+        bw.write(" \\. \". L_r'\n");
+        bw.write("   `~\\/\n");
+        bw.write("      |\n");
+        bw.write("      |\n");
+
+        bw.flush();
+        bw.close();
+    }
+}
