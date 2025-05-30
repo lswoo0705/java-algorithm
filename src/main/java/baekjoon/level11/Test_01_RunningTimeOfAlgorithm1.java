9e6c9576f32e1877d5b8f132d27337112d1f31c0
sangwoo lee <lswoo0705@gmail.com>
2025-05-30 10:40:53 +0900

    백준 11-1

---
+++ src/main/java/baekjoon/level11/Test_01_RunningTimeOfAlgorithm1.java
@@ -0,0 +1,17 @@
+package baekjoon.level11;
+
+import java.io.*;
+
+//MenOfPassion(A[], n) {
+//    i = ⌊n / 2⌋;
+//    return A[i]; # 코드1
+//}
+
+// 수행 횟수와 알고리즘의 최고차항의 차수를 출력
+
+public class Test_01_RunningTimeOfAlgorithm1 {
+    public static void main(String[] args) throws IOException {
+        System.out.println(1);
+        System.out.println(0);
+    }
+}
